34620f8caa042ff467eaa117bbb5b1c8fb82aabc
Pratham Garg <prathamgarg0445@gmail.com>
2025-06-17 21:06:20 +0530

    Feat/added-problem-model

---
+++ Problem_Service/src/models/Problem.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const ProblemSchema = new mongoose.Schema({
+    title : {
+        type: String,
+        required: [true, 'Title cannot be empty']
+     },
+     description: {
+        type: String,
+        required: [true, 'Description cannot be empty']
+     },
+     difficulty: {
+        type: String,
+        required: [true, 'Difficulty cannot be empty'],
+        enum : [`easy`, `medium`, `difficulty`],
+        default: 'easy'
+     },
+     constraints: {
+        type: String
+     },
+     sampleInput: {
+        type: String,
+        required: true
+     },
+     sampleOutput: {
+        type: String,
+        required: true
+     }
+});
+
+export const Problem = mongoose.model('Problem', ProblemSchema);
