5bca88d1df48d2fd2cdeff81e90f7419d1465525
Pratham Garg <prathamgarg0445@gmail.com>
2025-06-24 12:37:37 +0530

    Feat/added validation layer using zod

--- Evaluator_Service/src/index.js
+++ Evaluator_Service/src/index.js
@@ -3,9 +3,17 @@ import { PORT } from './config/serverConfig.js';
 import SampleQueueProducer from './producers/SampleQueueProducer.js';
 import SampleWorker from './workers/SampleWorker.js';
 import bullBoardAdapter from './config/bullBoardConfig.js';
+import apiRouter from './routes/apiRouter.js';
 
 const app = express();
 app.use('/ui', bullBoardAdapter.getRouter());
+
+app.use(express.json());
+app.use(express.raw());
+app.use(express.urlencoded({extended: true}));
+app.use(express.text());
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Server running at ${PORT}`);
