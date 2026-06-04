1a9ba37fee435689976d7a23ee1793fc91f166ed
Pratham Garg <prathamgarg0445@gmail.com>
2026-06-04 23:51:20 +0530

    cookie set to secure true is userController

--- User_Service/src/controllers/userController.js
+++ User_Service/src/controllers/userController.js
@@ -27,7 +27,7 @@ export const login = async(req, res) => {
             maxAge: 1000*60*60,
             httpOnly: true,
             sameSite: 'none',
-            secure: false
+            secure: true
         });
         return successResponse(
             { user : { id: user._id, email: user.email,username:user.username, role: user.role}},
