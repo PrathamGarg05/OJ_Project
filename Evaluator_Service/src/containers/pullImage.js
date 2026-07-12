b4106d383568890148a86f8752ed9dcbe43ea181
Pratham Garg <prathamgarg0445@gmail.com>
2026-07-12 21:18:07 +0530

    fix: skip network pull if image already cached locally

--- Evaluator_Service/src/containers/pullImage.js
+++ Evaluator_Service/src/containers/pullImage.js
@@ -1,13 +1,25 @@
+// pullImage.js
 import Docker from 'dockerode';
 
-export default function pullImage(imageName) {
-    const docker = new Docker();
-    return new Promise((res, rej) => {
-        docker.pull(imageName, (err, stream) => {
-            if (err) return rej(err);
-            docker.modem.followProgress(stream, (err, response) => 
-                err ? rej(err) : res(response)
-            );
+const docker = new Docker(); // reuse a single client, same fix as containerFactory
+
+export default async function pullImage(imageName) {
+    try {
+        const images = await docker.listImages({ filters: { reference: [imageName] } });
+        if (images.length > 0) {
+            return; // already present locally, skip network pull entirely
+        }
+
+        return new Promise((res, rej) => {
+            docker.pull(imageName, (err, stream) => {
+                if (err) return rej(err);
+                docker.modem.followProgress(stream, (err, response) =>
+                    err ? rej(err) : res(response)
+                );
+            });
         });
-    });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
