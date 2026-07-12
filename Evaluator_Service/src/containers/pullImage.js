f4bf3a6389ad227fb7415bedaa832f620fcf2282
Pratham Garg <prathamgarg0445@gmail.com>
2026-07-12 16:24:09 +0530

    fix: docker issue which crashed the whole eval service

--- Evaluator_Service/src/containers/pullImage.js
+++ Evaluator_Service/src/containers/pullImage.js
@@ -1,17 +1,13 @@
 import Docker from 'dockerode';
 
 export default function pullImage(imageName) {
-    try{
-        const docker = new Docker();
-        return new Promise((res,rej) => {
-            docker.pull(imageName, (err, stream) => {
-                if(err) throw err;
-                docker.modem.followProgress(stream, (err, response) => err ? rej(err) : res(response), (event) => {
-                });
-            });
+    const docker = new Docker();
+    return new Promise((res, rej) => {
+        docker.pull(imageName, (err, stream) => {
+            if (err) return rej(err);
+            docker.modem.followProgress(stream, (err, response) => 
+                err ? rej(err) : res(response)
+            );
         });
-    } catch(error){
-        console.log(error);
-        throw error;
-    }
+    });
 }
